6eac090413194fe253167092d86bb0650b1d844e
Effie <effiedesign.sf@gmail.com>
2017-11-16 00:42:09 -0800

    test - adding video function

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,18 @@
 // document ready
 $(function(){
-	
+	var video = document.querySelector("#videoElement");
+ 
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+	 
+	if (navigator.getUserMedia) {       
+	    navigator.getUserMedia({video: true}, handleVideo, videoError);
+	}
+	 
+	function handleVideo(stream) {
+	    video.src = window.URL.createObjectURL(stream);
+	}
+	 
+	function videoError(e) {
+	    console.log = "oops";
+	}
 })
